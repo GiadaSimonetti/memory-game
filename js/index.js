5ec5a6058e5cf63b758b49fae29bcfebf64e2b2e
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-07-19 20:11:21 +0100

    Add front and back + Rotate card

--- js/index.js
+++ js/index.js
@@ -68,20 +68,24 @@ gameGrid.sort(() => 0.5 - Math.random());
 
 // For each item in the cardsArray array...
 gameGrid.forEach(item => {
-  // Create a div
+  // Create card element with the name dataset
   const card = document.createElement('div');
-
-  // Apply a card class to that div
   card.classList.add('card');
-
-  // Set the data-name attribute of the div to the cardsArray name
   card.dataset.name = item.name;
 
-  // Apply the background image of the div to the cardsArray image
-  card.style.backgroundImage = `url(${item.img})`;
+  // Create front of card
+  const front = document.createElement('div');
+  front.classList.add('front');
 
-  // Append the div to the grid section
+  // Create back of card, which contains
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = `url(${item.img})`;
+
+  // Append card to grid, and front and back to each card
   grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
 });
 
 let count = 0;
@@ -112,13 +116,13 @@ grid.addEventListener('click', function (event) {
   if (count < 2) {
       count++;
       if (count === 1) {
-        // Assign first guess
-        firstGuess = clicked.dataset.name;
-        clicked.classList.add('selected');
+        firstGuess = clicked.parentNode.dataset.name;
+        console.log(firstGuess);
+        clicked.parentNode.classList.add('selected');
       } else {
-        // Assign second guess
-        secondGuess = clicked.dataset.name;
-        clicked.classList.add('selected');
+        secondGuess = clicked.parentNode.dataset.name;
+        console.log(secondGuess);
+        clicked.parentNode.classList.add('selected');
       }
       // If both guesses are not empty...
       if (firstGuess !== '' && secondGuess !== '') {
